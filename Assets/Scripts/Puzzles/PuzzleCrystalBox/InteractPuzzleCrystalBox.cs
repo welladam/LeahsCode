10301fd84f7a3583e50cd70141669f1745ccb5c4
welladam <wellington.f.adam@gmail.com>
2018-09-04 00:07:45 -0300

    Adicionado texto para Pressionar o botão E. Iniciado desenvolvimento para focar a camera.

---
+++ Assets/Scripts/Puzzles/PuzzleCrystalBox/InteractPuzzleCrystalBox.cs
@@ -0,0 +1,52 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit3D;
+
+[RequireComponent(typeof(Collider))]
+public class InteractPuzzleCrystalBox : MonoBehaviour {
+
+    public LayerMask layers;
+    public TextMesh textButton;
+    public Transform objectOnFocus;
+    public CameraSettings cameraRig;
+
+    private bool hasPlayerInArea;
+
+	// Use this for initialization
+	void Start () {
+        this.textButton.gameObject.SetActive(false);
+        this.hasPlayerInArea = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Interact") && hasPlayerInArea)
+        {
+            cameraRig.follow = objectOnFocus;
+            cameraRig.lookAt = objectOnFocus;
+            cameraRig.keyboardAndMouseCamera.Priority = 0;
+            cameraRig.controllerCamera.Priority = 1;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (0 != (layers.value & 1 << other.gameObject.layer))
+        {
+            this.textButton.gameObject.SetActive(true);
+            this.hasPlayerInArea = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (0 != (layers.value & 1 << other.gameObject.layer))
+        {
+            this.textButton.gameObject.SetActive(false);
+            this.hasPlayerInArea = false;
+        }
+    }
+}
